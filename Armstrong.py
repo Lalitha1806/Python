dd0fa36491f515c16b3c8cadc8ed01d7b3148764
Lalitha1806 <lalithaprasannakunapareddy@gmail.com>
2025-03-18 15:19:31 +0530

    Create Armstrong.py

---
+++ Armstrong.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a number: "))
+sum = 0
+temp = a
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10  
+    
+if a == sum:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
+    
